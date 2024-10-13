1cfa5b279825d26256ab67a5fbc85e1d64202037
nanaiwamoto <c5ijwtbwytn@gmail.com>
2024-10-13 16:45:45 +0900

    画像表示の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,39 @@
+class MessagesController < ApplicationController
+
+  before_action :set_room, only: [:create]
+  
+  
+  def create
+  
+  @message = @room.messages.new(message_params)
+  
+  if @message.save
+  
+  redirect_to @room
+  
+  else
+  
+  render 'new'
+  
+  end
+  
+  end
+  
+  
+  private
+  
+  def set_room
+  
+  @room = Room.find(params[:room_id])
+  
+  end
+  
+  def message_params
+  
+  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  
+  end
+  #上記はAIのオススメで入れたコード
 private
 
   def message_params
