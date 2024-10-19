4f4833c3ec503960e2a360ff26c163e927ed3f3b
nanaiwamoto <c5ijwtbwytn@gmail.com>
2024-10-19 18:11:56 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,24 @@
 class MessagesController < ApplicationController
-
-  before_action :set_room, only: [:create]
+  def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+  end
 
   def create
+    @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to @room
+      redirect_to room_messages_path(@room)
     else
-      render 'new'
+      @messages = @room.messages.includes(:user)
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
 
-  def set_room
-    @room = Room.find(params[:room_id])
-  end
-
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
